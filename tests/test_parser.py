3528e58bce3f4536a660616b4a500b2c60c0d58b
Johan FF <johan.artmat@gmail.com>
2023-07-22 22:28:54 -0500

    [CREATION] parser test, program test and letstatement test

---
+++ tests/test_parser.py
@@ -0,0 +1,56 @@
+from typing import (List, cast)
+from unittest import TestCase
+
+from lpp.ast import (
+  LetStatement,
+  Program,
+)
+from lpp.lexer import Lexer
+from lpp.parser import Parser
+
+class ParserTest(TestCase):
+  def test_parse_program(self) -> None:
+    source: str = 'variable x = 5;'
+    lexer: Lexer = Lexer(source)
+    parser: Parser = Parser(lexer)
+
+    program: Program = parser.parse_program()
+
+    self.assertIsNotNone(program)
+    self.assertIsInstance(program, Program)
+
+  def test_let_statements(self) -> None:
+    source: str = '''
+      variable x = 5;
+      variable y = 10;
+      variable foo = 20;
+    '''
+    lexer: Lexer = Lexer(source)
+    parser: Parser = Parser(lexer)
+
+    program: Program = parser.parse_program()
+
+    self.assertEqual(len(program.statements), 3)
+    for statement in program.statements:
+      self.assertEqual(statement.token_literal(), 'variable')
+      self.assertIsInstance(statement, LetStatement)
+
+  def test_names_in_let_statements(self) -> None:
+    source: str = '''
+      variable x = 5;
+      variable y = 10;
+      variable foo = 20;
+    '''
+    expected_identifiers: List[str] = ['x','y','foo']
+    lexer: Lexer = Lexer(source)
+    parser: Parser = Parser(lexer)
+
+    program: Program = parser.parse_program()
+
+    identifiers: List[str] = []
+    for statement in program.statements:
+      statement = cast(LetStatement, statement)
+      assert statement.name is not None
+      identifiers.append(statement.name.value)
+
+    self.assertEqual(identifiers, expected_identifiers)
